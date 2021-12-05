984cf5886f119562a3f9023f357dea98a8fe1013
CS-PRO1 <csprocsgo@gmail.com>
2021-12-05 16:36:22 +0200

    Added Category Linked Product to both Category and user ID

--- database/migrations/2021_12_05_101609_create_products_table.php
+++ database/migrations/2021_12_05_101609_create_products_table.php
@@ -16,6 +16,8 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->foreignId('category_id')->constrained('categories')->cascadeOnDelete();
+            $table->foreignId('owner_id')->constrained('users')->cascadeOnDelete();
             $table->longText('image_link');
             $table->date('expiration_date');
             $table->text('contact_info');
